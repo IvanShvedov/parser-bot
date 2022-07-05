76deb22357ed06d77bcc50f31b5ec71a20d70976
IvanShvedov <shvedov540@gmail.com>
2022-07-05 11:59:50 +0300

    delete chat handler added

--- app/handlers/chat.py
+++ app/handlers/chat.py
@@ -23,6 +23,28 @@ async def add_new_chat(message: types.Message):
             text="Что то пошло не так",
             chat_id=message.chat.id
         )
+
+async def delete_chat(message: types.Message):
+    chat_service = ChatControlService()
+    chat_id = message.get_args()
+    if chat_id is None or chat_id == "":
+        await chat_service.bot.send_message(
+            text="Отправь мне id чата через команду /del и я удалю его из базы\n - /del 12345678910",
+            chat_id=message.chat.id    
+        )
+        return
+    try:
+        await chat_service.delete(chat_id)
+        chat_service.bot.send_message(
+            text=f"{chat_id} успешно удален",
+            chat_id=message.chat.id
+        )
+    except Exception as e:
+        chat_service.bot.send_message(
+            text="Что то пошло не так",
+            chat_id=message.chat.id
+        )
+        
     # message_id = await chat_service.bot.copy_message(
     #     chat_id='-1001544120674',
     #     from_chat_id = message.chat.id,
